36218e78108fa8f1abd38e0a596560750f302bbb
Rahul Shakya <rs373725@gmail.com>
2024-06-11 11:54:12 +0545

    program commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("First Name: ");
+            var name1 = Console.ReadLine();
+            Console.Write("Last Name: ");
+            var name2 = Console.ReadLine();
+            var name3 = $"The full name is {name1} {name2}";
+            Console.WriteLine($"The full name is {name1} {name2}");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
