3ff778e39a6aece71409580f256732f3033097b1
Rahul Shakya <rs373725@gmail.com>
2024-06-11 12:20:15 +0545

    var changed to int

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.Write("First Name: ");
-            var name1 = Console.ReadLine();
+            int name1 = int.Parse(Console.ReadLine());
             Console.Write("Last Name: ");
             var name2 = Console.ReadLine();
             var name3 = $"The full name is {name1} {name2}";
