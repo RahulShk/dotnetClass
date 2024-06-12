3512d78991f2d9ac6acf0d30f57b2d219b38805f
Rahul Shakya <rs373725@gmail.com>
2024-06-12 22:10:35 +0545

    getter setter for car

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Car
+    {
+        public int CarId { get; set; }
+        public string CarName {  get; set; }
+        public string CarDescription {  get; set; }
+        public float CarSpeed {  get; set; }
+        public float CarPrice {  get; set; }
+
+/*        public Car(){}
+
+        public Car(int carId, string carName, string carDescription, float carSpeed, float carPrice)
+        {
+            this.CarId = carId;
+            this.CarName = carName;
+            this.CarDescription = carDescription;
+            this.CarSpeed = carSpeed;
+            this.CarPrice = carPrice;
+        }*/
+
+/*        public void setCarId(int carId)
+        {
+            this.CarId = carId;
+        }
+        public void setCarName(string carName)
+        {
+            this.CarName = carName;
+        }
+        public void setCarDescription(string carDescription)
+        {
+            this.CarDescription = carDescription;
+        }
+        public void setCarSpeed(float carSpeed)
+        {
+            this.CarSpeed = carSpeed;
+        }
+        public void setCarPrice(float carPrice)
+        {
+            this.CarPrice = carPrice;
+        }
+
+        public int getCarId()
+        {
+            return this.CarId;
+        }
+        public string getCarName()
+        {
+            return this.CarName;
+        }
+        public string getCarDescription()
+        {
+            return this.CarDescription;
+        }
+        public float getCarSpeed()
+        {
+            return this.CarSpeed;
+        }
+        public float getCarPrice()
+        {
+            return this.CarPrice;
+        }*/
+    }
+}
