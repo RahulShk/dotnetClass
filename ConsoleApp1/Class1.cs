d2b44f01b7858150987294bb164b1ac21185d8e6
Rahul Shakya <rs373725@gmail.com>
2024-06-13 13:43:43 +0545

    print all car details.

--- ConsoleApp1/Class1.cs
+++ ConsoleApp1/Class1.cs
@@ -9,12 +9,12 @@ namespace ConsoleApp1
     public class Car
     {
         public int CarId { get; set; }
-        public string CarName {  get; set; }
-        public string CarDescription {  get; set; }
-        public float CarSpeed {  get; set; }
-        public float CarPrice {  get; set; }
+        public string CarName { get; set; }
+        public string CarDescription { get; set; }
+        public float CarSpeed { get; set; }
+        public float CarPrice { get; set; }
 
-/*        public Car(){}
+        public Car() { }
 
         public Car(int carId, string carName, string carDescription, float carSpeed, float carPrice)
         {
@@ -23,48 +23,15 @@ namespace ConsoleApp1
             this.CarDescription = carDescription;
             this.CarSpeed = carSpeed;
             this.CarPrice = carPrice;
-        }*/
-
-/*        public void setCarId(int carId)
-        {
-            this.CarId = carId;
-        }
-        public void setCarName(string carName)
-        {
-            this.CarName = carName;
-        }
-        public void setCarDescription(string carDescription)
-        {
-            this.CarDescription = carDescription;
-        }
-        public void setCarSpeed(float carSpeed)
-        {
-            this.CarSpeed = carSpeed;
-        }
-        public void setCarPrice(float carPrice)
-        {
-            this.CarPrice = carPrice;
         }
 
-        public int getCarId()
+        public override string ToString()
         {
-            return this.CarId;
+            return $"CarId: {CarId}\n" +
+                $" CarName: {CarName}\n" +
+                $" CarDescription: {CarDescription}\n" +
+                $" CarSpeed: {CarSpeed} km/h\n" +
+                $" CarPrice: ${CarPrice}";
         }
-        public string getCarName()
-        {
-            return this.CarName;
-        }
-        public string getCarDescription()
-        {
-            return this.CarDescription;
-        }
-        public float getCarSpeed()
-        {
-            return this.CarSpeed;
-        }
-        public float getCarPrice()
-        {
-            return this.CarPrice;
-        }*/
     }
 }
